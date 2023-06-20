a56ec0f3db68e1874a25c31aa8718cda3797a371
octa-acostamonti <acostamonti.business@gmail.com>
2023-06-20 18:53:11 -0300

    ñomi

---
+++ app.py
@@ -0,0 +1,10 @@
+import streamlit as st
+
+st.set_page_config(page_title="MOOC´s ANALISIS",page_icon=":tada:")
+
+# ----CABECERA----#
+
+st.subheader("Este es un proyecto de analisis sobre base de dato de MOOC´s")
+st.title("Hola")
+st.write("How are you men wtf?")
+
