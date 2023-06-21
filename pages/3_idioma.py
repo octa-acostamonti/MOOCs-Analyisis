f8aec6f7c7684af8f5e66c197f18db72a43a4a30
octa-acostamonti <acostamonti.business@gmail.com>
2023-06-21 11:04:23 -0300

    modified

--- pages/3_idioma.py
+++ pages/3_idioma.py
@@ -1,1 +1,22 @@
 import streamlit as st
+import pandas as pd
+import matplotlib.pyplot as plt
+import seaborn as sns
+
+edx = pd.read_csv("./Datasets_normalizados/edx_normalizado.csv").drop(columns="Unnamed: 0")
+
+edx_lan = edx[["language","n_enrolled"]]
+edx_lan.loc[6, 'language'] = 'Japones'
+edx_lan.loc[7, 'language'] = 'Chino'
+
+edx_language = edx_lan.groupby("language")["n_enrolled"].sum()
+edx_language = edx_language.to_frame().reset_index()
+edx_language.sort_values("n_enrolled",ascending=False)
+
+
+fig, ax = plt.subplots()
+sns.barplot(data=edx_language, x="language", y="n_enrolled", saturation=1, ax=ax)
+sns.despine(left=True, bottom=True)
+ax.set_xlabel("Numero de Suscriptores")
+ax.set_ylabel("Idioma")
+st.pyplot(fig)  
--- pages/6_WordClouds.py
+++ pages/6_WordClouds.py
@@ -1,1 +1,45 @@
 import streamlit as st
+import pandas as pd
+from wordcloud import WordCloud
+import matplotlib.pyplot as plt
+coursera_completo = pd.read_csv("./Datasets_normalizados/coursera_normalizado.csv").drop(columns="Unnamed: 0")
+edx = pd.read_csv("./Datasets_normalizados/edx_normalizado.csv").drop(columns="Unnamed: 0")
+udemy = pd.read_csv("./Datasets_normalizados/udemy_normalizado.csv").drop(columns="Unnamed: 0")
+
+
+st.header("WordClouds")
+st.divider()
+st.markdown("##### Coursera:")
+
+coursera_titulos = ' '.join(coursera_completo['name'])
+wordcloud = WordCloud(width=800, height=400, background_color='white').generate(coursera_titulos)
+
+# Display WordCloud in Streamlit
+fig, ax = plt.subplots(figsize=(10, 6))
+plt.imshow(wordcloud, interpolation='bilinear')
+plt.axis('off')
+st.pyplot(fig)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
